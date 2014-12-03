2591911b67150f0874550ca757ba751461c77ace
Bobby Seidensticker <bseidensticker@gmail.com>
2014-12-02 21:54:37 -0800

    Added some sugar, append and iterate. Added Subject pattern, basic testing by making a CharModel inheriting from Subject, TODO: CharHeaderView holds a CharModel and subscribes to changes and updates its html accordingly

---
+++ scripts/sugar.js
@@ -0,0 +1,17 @@
+Array.prototype.append = function(v) {
+    this[this.length] = v;
+}
+
+function iterate(obj, cb) {
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            cb(prop, obj[prop]);
+        }
+    }
+}
+
+function assert(statement) {
+    if (!statement) {
+        throw('Assertion error');
+    }
+}

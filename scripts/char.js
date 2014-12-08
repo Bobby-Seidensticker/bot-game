5afdcf3157f155372a1b4c2a9a2efa9450669b77
Bobby Seidensticker <bseidensticker@gmail.com>
2014-12-08 13:34:49 -0800

    check

--- scripts/char.js
+++ scripts/char.js
@@ -5,8 +5,29 @@ namespace.module('bot.char', function (exports, require) {
     var log = namespace.bot.log;
 
     var CharModel = Backbone.Model.extend({
+        defaults: {
+            str: 5,
+            intel: 5
+        },
+
+        initialize: function() {
+            console.log(this.get('fuckyou'));  // prints 'hey'
+        },
+
+        computeAttrs: function() {
+            var hp = this.str * 10;
+            var mana = this.intel * 10;
+            this.set({
+                hp: hp,
+                maxHp: hp,
+                mana: mana,
+                maxMana: mana
+            });
+        },
         
     });
+
+    var c = new CharModel({ str: 10, intel: 20, fuckyou: 'hey' });
 
 
     exports.extend({

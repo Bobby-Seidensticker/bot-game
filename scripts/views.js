e492d1e8d9fd2cca17bfbd410badb8bca3a83be7
Bobby Seidensticker <bseidensticker@gmail.com>
2014-12-17 10:16:14 -0800

    Header now reponds to equipped gear changes

--- scripts/views.js
+++ scripts/views.js
@@ -13,15 +13,34 @@ namespace.module('bot.views', function (exports, require) {
 
             this.$el.html(this.template(_.extend({ items: this.char.get('equipped').toDict() }, this.char.toJSON())));
 
-            this.setElement(this.$('.stats'));
+            //this.setElement(this.$('.stats'));
 
             this.char.on('change', this.render, this);
+
+            var slots = this.char.get('equipped').slots;
+            this.slotImages = _.object(slots,
+                                         _.map(slots, function(slot) {
+                                             return this.$('.' + slot + ' .slot-img');
+                                         }, this)
+                                        );
         },
 
         render: function() {
             this.$('#hp').html(Math.ceil(this.char.get('hp')) + ' / ' + this.char.get('maxHp'));
             this.$('#mana').html(Math.floor(this.char.get('mana')) + ' / ' + this.char.get('maxMana'));
             this.$('#xp').html(Math.floor(this.char.get('xp')) + ' / ' + this.char.get('nextLevelXp'));
+
+            var items = this.char.get('equipped').toDict();
+
+            _.each(items, function(item, slot) {
+                var $img = this.slotImages[slot];
+                if (item) {
+                    $img.removeClass('empty');
+                    $img.attr('src', 'assets/' + item.get('name') + '.svg');
+                } else {
+                    $img.addClass('empty');
+                }
+            }, this);
         },
     });
 

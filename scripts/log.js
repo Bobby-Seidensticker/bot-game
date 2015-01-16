ad9b26851d12a7b1ca837014eab75fc5a8a64569
Bobby Seidensticker <bseidensticker@gmail.com>
2015-01-15 16:28:21 -0800

    Got line numbers in logs, awesome

--- scripts/log.js
+++ scripts/log.js
@@ -2,9 +2,9 @@ namespace.module('bot.log', function (exports, require) {
 
     var LEVEL = 'info';
 
-    var FNS = [debug, info, warning, error, line];
+    var FNS = [debug, info, warning, error, stack];
 
-    var NAMES = ['debug', 'info', 'warning', 'error', 'line'];
+    var NAMES = ['debug', 'info', 'warning', 'error', 'stack'];
 
     var extender = {};
     var clear = false;
@@ -21,38 +21,43 @@ namespace.module('bot.log', function (exports, require) {
 
     exports.extend(extender);
 
+    function fileLine() {
+        var s = new Error().stack.split('\n')[3];
+        return s.slice(s.indexOf('(') + 1, s.length - 1);
+    }
+
     function dateStr() {
         return (new Date()).toString().slice(4, -15);
     }
 
     function debug() {
         var a = arguments;
-        a[0] = 'DEBUG ' + dateStr() + ' ' + a[0];
-        console.log("%c" + sprintf.apply(null, a), "color:blue");
+        a[0] = 'DEBUG ' + fileLine() + ' ' + a[0];
+        console.log('%c' + sprintf.apply(null, a), 'color: blue');
     }
 
     function info() {
         var a = arguments;
-        a[0] = 'INFO ' + dateStr() + ' ' + a[0];
-        console.log("%c" + sprintf.apply(null, a), "color: green");
+        a[0] = 'INFO ' + fileLine() + ' ' + a[0];
+        console.log('%c' + sprintf.apply(null, a), 'color: green');
     }
 
     function warning() {
         var a = arguments;
-        a[0] = 'WARNING ' + dateStr() + ' ' + a[0];
-        console.log("%c" + sprintf.apply(null, a), "color: orange");
+        a[0] = 'WARNING ' + fileLine() + ' ' + a[0];
+        console.log('%c' + sprintf.apply(null, a), 'color: orange');
     }
 
     function error() {
         var a = arguments;
-        a[0] = 'ERROR ' + dateStr() + ' ' + a[0];
-        console.log("%c" + sprintf.apply(null, a), "color: red");
+        a[0] = 'ERROR ' + fileLine() + ' ' + a[0];
+        console.log('%c' + sprintf.apply(null, a), 'color: red');
     }
 
-
-    //  call with "log.line(new Error(), "your text here");
-    function line() {
-        e = arguments[0];
-        console.log("%c" + arguments[1] + " %s", "color:purple", e.stack);
+    //  call with 'log.line(new Error(), 'your text here');
+    function stack() {
+        var a = arguments;
+        a[0] = new Error().stack.replace(/   at /g, '').split('\n').slice(2).join('\n') + '\n  ' + a[0];
+        console.log('%c' + sprintf.apply(null, a), 'color: purple');
     }
 });
